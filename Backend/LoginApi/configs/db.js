95e602ff7e139727ae3f5b0d345ee75838dec667
Deepak Vats <deepakvats123123@gmail.com>
2022-12-30 18:32:04 +0530

    Login API

--- Backend/LoginApi/configs/db.js
+++ Backend/LoginApi/configs/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connect = () => {
-    return mongoose.connect('mongodb://localhost:27017/web6_ecommerce')
+    return mongoose.connect('mongodb://localhost:27017/loginapi')
 }
 
 module.exports = connect;
