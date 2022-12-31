1be0c4cb18a223f92645b8afa92629be44091080
Deepak Vats <deepakvats123123@gmail.com>
2022-12-31 20:02:09 +0530

    add login api

---
+++ Backend/LoginApi/controller/userController.js
@@ -0,0 +1,42 @@
+import oyouser from "../models/userModels.js";
+import jwt from "jsonwebtoken";
+
+const jsontoken = (user) => {
+  return jwt.sign({ user }, "confidential");
+};
+// Registration started
+export const Signup = async (req, res) => {
+  try {
+    let user = await oyouser.findOne({ email: req.body.email });
+    if (user) return res.status(400).send({ message: "User is already exist" });
+    user = await oyouser.create(req.body);
+    return res.status(201).send({ email: user.email, id: user._id });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
+// Login started
+
+export const Login = async (req, res) => {
+  try {
+    const user = await oyouser.findOne({ email: req.body.email });
+
+    if (!user)
+      return res
+        .status(404)
+        .send({ message: "Please enter valid email and password" });
+
+    const match = user.checkPassword(req.body.password);
+
+    if (!match)
+      return res
+        .status(404)
+        .send({ message: "Please enter valid email and password" });
+
+    const token = jsontoken(user);
+
+    return res.status(200).send({ token: token });
+  } catch (error) {
+    return res.status(500).send(error.message);
+  }
+};
--- Backend/LoginApi/index.js
+++ Backend/LoginApi/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const connect = require('./configs/db');
-const { register, login } = require('./controllers/auth.controller');
-const cors = require('cors');
+import express from "express";
+import connection from "./config/db.js";
+import mongoose from "mongoose";
+
+const port = process.env.PORT || 5050;
+mongoose.set("strictQuery", false);
+import cors from "cors";
+import { Login} from "./controller/userController.js";
+import { Signup } from "./controller/userController.js";
 
 const app = express();
-
 app.use(express.json());
 app.use(cors());
-
-app.post('/register', register)
-app.post('/login', login)
-
-const port = 8080;
-
-app.listen(port, async (req, res) => {
-    try {
-        await connect();
-        console.log(`Server is running on port ${port}`);   
-    } catch (error) {
-        console.log(error.message);
-    }
-})
+app.post("/signup", Signup);
+app.post("/login", Login);
+app.listen(port, () => {
+  try {
+    console.log("Listening on Port");
+    connection();
+  } catch (e) {
+    console.log(e);
+  }
+});
